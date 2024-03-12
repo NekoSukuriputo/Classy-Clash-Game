c241e37e8ac5e003f828032caedfd7006273a107
abdul <abdul.rahmat@360kredi.id>
2024-03-12 14:06:41 +0700

    75. Inheriting From BaseCharacter

--- BaseCharacter.h
+++ BaseCharacter.h
@@ -1,10 +1,36 @@
+
 #ifndef BASE_CHARACTER_H
 #define BASE_CHARACTER_H
 
+#include "raylib.h"
 class BaseCharacter
 {
 public:
     BaseCharacter();
+    Vector2 getWorldPos() { return worldPos; }
+    void undoMovement();
+    Rectangle getCollisionRec();
+
+protected:
+    Texture2D texture{LoadTexture("characters/knight_idle_spritesheet.png")};
+    Texture2D idle{LoadTexture("characters/knight_idle_spritesheet.png")};
+    Texture2D run{LoadTexture("characters/knight_run_spritesheet.png")};
+    Vector2 screenPos{};
+    Vector2 worldPos{};
+    Vector2 worldPosLastFrame{};
+    // 1 : facing right, -1: facing left
+    float rightLeft{1.f};
+
+    // animation ini
+    float runningTime{};
+    int frame{};
+    const int maxFrames{6};
+    const float updateTime{1.f / 12.f};
+    const float speed{4.f};
+    float width{};
+    float height{};
+    float scale{4.0f};
+
 private:
 };
 
