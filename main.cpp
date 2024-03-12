c96e2f10a2bb3526828cd3e16009fbc8903ce727
abdul <abdul.rahmat@360kredi.id>
2024-03-12 10:07:59 +0700

    65. The Tick Function

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@
 class Character
 {
 private:
-    Texture2D texture;
-    Texture2D idle;
-    Texture2D run;
+    Texture2D texture{LoadTexture("characters/knight_idle_spritesheet.png")};
+    Texture2D idle{LoadTexture("characters/knight_idle_spritesheet.png")};
+    Texture2D run{LoadTexture("characters/knight_run_spritesheet.png")};
     Vector2 screenPos;
     Vector2 worldPos;
     // 1 : facing right, -1: facing left
@@ -17,10 +17,55 @@ private:
     int frame{};
     const int maxFrame{6};
     const float updateTime{1.f / 12.f};
+    const float speed{4.f};
 
 public:
     Vector2 getWorldPos() { return worldPos; }
+    void setScreenPos(int winWidth, int winHeigth);
+    void tick(float deltaTime);
 };
+
+void Character::setScreenPos(int winWidth, int winHeigth)
+{
+    screenPos = {
+        (float)winWidth / 2.0f - 4.0f * (0.5f * (float)texture.width / 6.0f),
+        (float)winHeigth / 2.0f - 4.0f * (0.5f * (float)texture.height)};
+}
+
+void Character::tick(float deltaTime)
+{
+    // map direction
+    Vector2 direction{};
+    if (IsKeyDown(KEY_A))
+        direction.x -= 1.0;
+    if (IsKeyDown(KEY_D))
+        direction.x += 1.0;
+    if (IsKeyDown(KEY_W))
+        direction.y -= 1.0;
+    if (IsKeyDown(KEY_S))
+        direction.y += 1.0;
+    if (Vector2Length(direction) != 0.0)
+    {
+        // set worldPos = worldPos + direction
+        worldPos = Vector2Add(worldPos, Vector2Scale(Vector2Normalize(direction), speed));
+
+        direction.x < 0.f ? rightLeft = -1.f : rightLeft = 1.f;
+        texture = run;
+    }
+    else
+    {
+        texture = idle;
+    }
+    // update animation frame
+    runningTime += deltaTime;
+    if (runningTime > updateTime)
+    {
+        frame++;
+        runningTime = 0.f;
+        if (frame > maxFrame)
+            frame = 0;
+    }
+}
 
 int main()
 {
